dfc14c9817c14165c3cae4c09ba1f89f01bcc49b
subrat008 <24cse041.subrattripathy@giet.edu>
2026-08-03 22:47:19 +0530

    Add files via upload

---
+++ DMDW-LAB-main/DMDW-LAB-main/Assignment-2/random (1).py
@@ -0,0 +1,11 @@
+import random as r
+num=r.random()
+print(num)
+
+list=[10,20,30,40,50,60,70,80,90,100]
+print(r.choice(list))
+
+print(r.choice([1,2,3,4,5,6,7,8,9,10]))
+print(r.randrange(10,5,3))
+r.seed(5)
+print(r.r())
